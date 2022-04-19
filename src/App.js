e6008d694748a0cb4448208675dc9a50289129b2
Aliaksandra Skachkova <skach.alexandra@gmail.com>
2022-04-19 21:43:05 +0200

    add icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import "./base.scss";
-
+import ArrowRightIcon from "./icons/ArrowRightIcon";
+import EditIcon from "./icons/EditIcon";
+import EyeClosedIcon from "./icons/EyeClosedIcon";
+import LockIcon from "./icons/LockIcon";
+import TrashIcon from "./icons/TrashIcon";
 function App() {
   return (
     <div className="App">
@@ -9,6 +13,11 @@ function App() {
       <p>test</p>
       <h2>TEST</h2>
       <h3>TEST</h3>
+      <button>Lets start !</button>
+      <TrashIcon></TrashIcon>
+      <EyeClosedIcon></EyeClosedIcon>
+      <EditIcon></EditIcon>
+      <ArrowRightIcon></ArrowRightIcon>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
