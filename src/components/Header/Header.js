630dbc5a6f5f19aa53a7caf48b1e62ea6b03792c
Aliaksandra Skachkova <skach.alexandra@gmail.com>
2022-05-12 21:43:09 +0200

    add header + styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Header.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Header.scss";
+import ArrowLeftIcon from "../../icons/ArrowLeftIcon";
 
 function Header(props) {
-  const isReturnButton = props.retour
+  const isReturnButton = props.retour;
   return (
-    <div>
-      <div className='title'>
-        Kindred {props.retour}
+    <div className="header">
+      <div>
+        <span className="logo">Kindred</span>
       </div>
-      {isReturnButton ? (
-        <>
-          Go back
-        </>
-      ) : (
-        ""
-      )}
+      <div>
+        {isReturnButton ? (
+          <button className="header__button">
+            <span className="header__button_icon">
+              <ArrowLeftIcon></ArrowLeftIcon>
+            </span>
+            Go back
+          </button>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 Header.propTypes = {};
