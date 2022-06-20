af68dca5a76d0e10369eab7c0ed4611a400a7f34
Aliaksandra Skachkova <skach.alexandra@gmail.com>
2022-06-20 21:19:26 +0200

    add other components

---
+++ components/icons/ArrowLeftIcon.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from "react";
+import { useState } from "react";
+
+function ArrowLeftIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.5 12L6.5 12M6.5 12L11.0882 16M6.5 12L11.0882 8"
+        stroke="#000A27"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+ArrowLeftIcon.propTypes = {};
+
+ArrowLeftIcon.defaultProps = {};
+
+export default ArrowLeftIcon;
---
+++ pages/parent-settings/parent-settings.js
@@ -0,0 +1,152 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Parent-Settings.scss";
+// import EditIcon from "../icons/EditIcon";
+import EditIcon from "../../../icons/EditIcon";
+import { Link, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { Formik, Field, Form } from "formik";
+import Header from "../../Header/Header";
+import Navbar from "../../navbar/Navbar";
+
+function ParentSettings() {
+  const location = useLocation();
+
+  const isSettingsPage = location.pathname === "/settings";
+  const isChangePasswordPage = location.pathname === "/change-password";
+  const isLogOutPage = location.pathname === "/logout";
+
+  return (
+    <div>
+      <Header retour="true"></Header>
+      <div className="container__profile">
+        {isSettingsPage ? (
+          <>
+            <div>
+              <img className="profile__picture" />
+              <div className="profile__name">Robert godwin</div>
+              <div className="profile__status">Parent</div>
+            </div>
+            <div>
+              <span className="profile__email">robertgodwin@mail.com</span>
+              {/* <a className='profile-edit-icon'><EditIcon></EditIcon></a> */}
+            </div>
+            <div>
+              <span className="profile__birthdate">february 20th 1972</span>
+              {/* <a className='profile-edit-icon'><EditIcon></EditIcon></a> */}
+            </div>
+            <button className="button-outline">Logout</button>
+            <p className="profile-link">
+              <Link to="/change-password">Change your password</Link>
+            </p>
+            <p className="profile-link">
+              <Link to="/logout">Delete your account</Link>
+            </p>
+          </>
+        ) : isChangePasswordPage ? (
+          <>
+            <h2>Changing your password</h2>
+            <h3>New password</h3>
+            <div className="form">
+              <Formik
+                initialValues={{
+                  password: "",
+                  password: "",
+                }}
+                validate={(values) => {
+                  const errors = {};
+                  if (!values.email) {
+                    errors.email = "Required";
+                  } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                      values.email
+                    )
+                  ) {
+                    errors.email = "Invalid email address";
+                  }
+                  return errors;
+                }}
+                onSubmit={async (values) => {
+                  fetch(process.env.REACT_APP_API + "/api/login", {
+                    method: "post",
+                    body: JSON.stringify(values),
+                    headers: {
+                      Accept: "application/json",
+                      "Content-Type": "application/json",
+                    },
+                  })
+                    .then((res) => res.json())
+                    .then(
+                      (result) => {
+                        if (result.access_token) {
+                          sessionStorage.setItem("isLoggedIn", true);
+                          sessionStorage.setItem("role", result.user.role);
+                          sessionStorage.setItem(
+                            "user",
+                            JSON.stringify(result)
+                          );
+                          // result.user.role === "admin"
+                          //   ? setRoleAdmin(true)
+                          //   : setIsSubmitted(true);
+                          // setIsSubmitted(true)
+                        } else {
+                          // setIsSubmitted(false);
+                          alert(
+                            "Error, please verify your user or your password"
+                          );
+                        }
+                      },
+                      (error) => {
+                        // setIsSubmitted(false);
+                        console.log(error);
+                      }
+                    );
+                }}
+              >
+                <Form>
+                  <div className="input-container">
+                    <Field
+                      type="text"
+                      name="password"
+                      placeholder="Enter your new password"
+                      required
+                    />
+                  </div>
+                  <div className="input-container">
+                    <Field
+                      type="text"
+                      name="password"
+                      placeholder="Confirm your new password"
+                      required
+                    />
+                  </div>
+                  {/* {renderErrorMessage("pass")}
+    {renderErrorMessage("email")} */}
+                  <div className="button-container">
+                    <button className="button-dark" type="submit">
+                      Sign in
+                    </button>
+                  </div>
+                </Form>
+              </Formik>
+            </div>
+          </>
+        ) : isLogOutPage ? (
+          <Navigate to="/" />
+        ) : (
+          ""
+        )}
+        <div className="navbar">
+          {" "}
+          <Navbar></Navbar>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ParentSettings.propTypes = {};
+
+ParentSettings.defaultProps = {};
+
+export default ParentSettings;
